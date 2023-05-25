07819ed1c982a353f4324f2760644d717b33dcaa
VladislavaSc <vladislava.fridmann@gmail.com>
2023-05-25 09:35:28 +0300

    Commit

--- RAF-9/raf9.py
+++ RAF-9/raf9.py
@@ -21,8 +21,29 @@ class Raf9:
         print('0 - exit')
 
     def choose_ingredients(self):
+        choose_ings = []
         print('Ingredient list: ')
 
+        i = 0
+        for ing in self.ingredients:
+            i += 1
+            print(f'{i}. {ing}')
+
+        print('0 - for exit')
+
+        while True:
+            command = input('Enter command: ')
+            if command == '0':
+                return choose_ings
+            else:
+                if command.isdigit():
+                    number = int(command)
+                    if number > len(self.ingredients):
+                        print('There is no such ingredient in the list')
+                    else:
+                        choose_ings.append(self.ingredients[number-1])
+                else:
+                    print('Enter ingredient number')
 
 if __name__ == '__main__':
     raf9 = Raf9()
