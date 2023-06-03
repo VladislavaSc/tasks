6f91082e5e3d8c735b7d2e11b223d4853378ddb9
VladislavaSc <vladislava.fridmann@gmail.com>
2023-06-03 20:30:59 +0300

    Commit

--- N2/interface_segregation.py
+++ N2/interface_segregation.py
@@ -35,18 +35,39 @@ class PaymentProcessor(ABC):
         pass
 
 
+class PaymentProcessorSMS(PaymentProcessor):
+
+    @abstractmethod
+    def pay(self, order):
+        pass
+
+    @abstractmethod
+    def auth_sms(self):
+        pass
+
+
 class DebitPaymentProcessor(PaymentProcessor):
 
     def __init__(self, security_code):
         self.security_code = security_code
+        self.is_verified = False
+
+    def auth_sms(self):
+        print(f'Security code check: {self.security_code}')
+        self.is_verified = True
 
     def pay(self, order):
+        if not self.is_verified:
+            raise Exception('Login by SMS')
         print('Processing debit payment type')
-        print(f'Security code check: {self.security_code}')
         order.status = 'paid'
 
 
 class CreditPaymentProcessor(PaymentProcessor):
+
+    def __init__(self, security_code):
+        self.security_code = security_code
+
     def pay(self, order):
         print('Processing credit payment type')
         print(f'Security code check: {self.security_code}')
@@ -57,10 +78,16 @@ class PaypalPaymentProcessor(PaymentProcessor):
 
     def __init__(self, email_address):
         self.email_address = email_address
+        self.is_verified = False
+
+    def auth_sms(self):
+        print(f'Security E-Mail: {self.email_address}')
+        self.is_verified = True
 
     def pay(self, order):
+        if not self.is_verified:
+            raise Exception('Login by SMS')
         print('Processing PayPal payment type')
-        print(f'Security E-Mail: {self.email_address}')
         order.status = 'paid'
 
 
@@ -71,10 +98,12 @@ order.add_item('USB', 2, 250)
 
 print(order.total_price())
 debit_processor = DebitPaymentProcessor('0372846')
+debit_processor.auth_sms()
 debit_processor.pay(order)
 
 credit_processor = CreditPaymentProcessor('0372846')
 credit_processor.pay(order)
 
-credit_processor = PaypalPaymentProcessor('some@mail.com')
-credit_processor.pay(order)
+paypal_processor = PaypalPaymentProcessor('some@mail.com')
+paypal_processor.auth_sms()
+paypal_processor.pay(order)
